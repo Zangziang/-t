d69302c64dd1b12430f6f536d4fe49576b379812
Zangziang <giangpthph33391@fpt.edu.vn>
2023-11-24 22:26:45 +0700

    first commit

---
+++ db/connect.php
@@ -0,0 +1,13 @@
+<?php
+$con = mysqli_connect("localhost","root","Giang2004@","bandienmay1");
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+	  // Change character set to utf8
+	mysqli_set_charset($con,"utf8");
+
+	
+?>
